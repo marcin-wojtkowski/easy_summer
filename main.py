5d13b670a2cd987fbc49a9c29dd4a5b214c792a9
Marcin Wojtkowski <marcinwojtkowski94@gmail.com>
2020-06-21 12:21:56 -0700

    Added subtraction, changed the main conditional branching in code to be a try except statement, and started using float as the main datatype instead of converting from str to int

--- main.py
+++ main.py
@@ -7,8 +7,10 @@
         2a
             Fix the bug where a string input will cause a compiler error.
         FIXED
-
-
+    Stage 3
+        Add subtraction
+            #converting user input into float made working with numbers much easier
+            instead of converting back and forth from string to int.
 '''
 
 print('Easy Summer')
@@ -17,21 +19,29 @@ sum = 0
 list = []
 
 while(True):
+
     variable = input()
 
-    if variable.isdigit():
+    try:
+        if variable == 'quit':
+            break
+        f = float(variable)
+
         if len(list) == 0:
-            list.append(variable)
+            list.append(f)
+        elif f < 0:
+            list.append(' - ' + str(abs(f)))
+            #elif exists to properly format summation history
         else:
-            list.append(' + ' + variable)
+            list.append(' + ' + str(f))
 
         for x in list:
             print(x, end = '')
 
-        sum += int(variable)
+        sum += f
 
-        print('\nSum: ' + str(sum))
-    elif variable == 'quit':
-        break
-    else:
-        print('Invalid input detected')
+        print('\nSum: ', sum)
+
+
+    except ValueError:
+        print('Invalid input')
